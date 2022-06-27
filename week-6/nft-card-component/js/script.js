bd20ff977acfa7adfbf17ba0f3b8d97390589e2f
Nasyikat Mambetova <nasyikat.mambetova.kk@gmail.com>
2022-06-28 04:19:57 +0600

    add more styles

--- week-6/nft-card-component/js/script.js
+++ week-6/nft-card-component/js/script.js
@@ -32,13 +32,16 @@ function createCard() {
     const card = document.createElement("div");
     card.className = "card";
     card.style.position = "relative";
-    card.style.display = "flex";
-    card.style.justifyContent = "center";
-    card.style.alignItems = "center";
-    card.style.boxSizing = "border-Box";
+    // card.style.display = "flex";
+    // card.style.flexWrap = "wrap";
+    // card.style.direction = "column";
+    // card.style.justifyContent = "center";
+    // card.style.alignItems = "center";
+    // card.style.boxSizing = "border-Box";
     card.style.width = "350px";
     card.style.height = "600px";
-    card.style.padding = "24px";
+    // card.style.padding = "0";
+    // card.style.margin = "0";
     card.style.marginLeft = "auto";
     card.style.marginRight = "auto";
     card.style.background = "hsl(216, 50%, 16%)";
@@ -49,16 +52,76 @@ function createCard() {
     cardIMg.className = "card-img";
     cardIMg.setAttribute('src', 'images/image-equilibrium.jpg');
     // cardIMg.src = 'images/image-equilibrium.jpg';
-    cardIMg.style.position = "relative"
+    // cardIMg.style.display = "flex";
+    // cardIMg.style.alignItems = "flex-start";
     cardIMg.style.width = "302px";
     cardIMg.style.height = "302px";
     cardIMg.style.borderRadius = "10px";
+    cardIMg.style.margin = "24px";
+    cardIMg.style.marginBottom = "24px";
+
+    // h3
+    const cardTitle = document.createElement("h3");
+    cardTitle.className = "card-title";
+    cardTitle.innerText = "Equilibrium #3429";
+    cardTitle.style.fontFamily = "Outfit";
+    cardTitle.style.fontSize = "22px";
+    cardTitle.style.fontStyle = "normal";
+    cardTitle.style.fontWeight = "700";
+    cardTitle.style.color = "hsl(0, 0%, 100%)";
+    cardTitle.style.margin = "0";
+    cardTitle.style.marginLeft = "24px";
+    cardTitle.style.padding = "0";
+
+    // subtitle
+    const subtitle = document.createElement("p");
+    subtitle.className = "subtitle";
+    subtitle.innerText = 'Our Equilibrium collection promotes balance and calm.';
+    subtitle.style.fontWeight = "400";
+    subtitle.style.fontSize = "18px";
+    subtitle.style.lineHeight = "26px";
+    subtitle.style.color = "hsl(215, 51%, 70%)";
+    subtitle.style.marginLeft = "24px";
+    subtitle.style.marginBottom = "24px";
+    
+    // img-info-flex
+    const imgInfo = document.createElement("div");
+    imgInfo.className = "img-info";
+    imgInfo.style.display = "flex";
+    // imgInfo.style.justifyContent = "center";
+    // imgInfo.style.gap = "111px";
+
+    // img-info-icon
+    const imgIcon = document.createElement("img");
+    imgIcon.className = "img-icon";
+    imgIcon.src = "images/icon-ethereum.svg";
+    imgIcon.style.marginLeft = "24px";
+    imgIcon.style.height = "18px";
+
+    // img-info-text
+    const imgInfoText = document.createElement("p");
+    imgInfoText.className = "img-info-text";    
+    imgInfoText.innerText = "0.041 ETH";
+    imgInfoText.style.margin = "0"
+    imgInfoText.style.fontFamily = "Outfit";
+    imgInfoText.style.fontStyle= "normal";
+    imgInfoText.style.fontWeight = "600";
+    imgInfoText.style.fontSize = "16px";
+    imgInfoText.style.color = "hsl(178, 100%, 50%)";
+    imgInfoText.style.marginLeft = "7px";
+
+
 
 
     document.body.appendChild(wrapper);
     wrapper.appendChild(container);
     container.appendChild(card);
     card.appendChild(cardIMg);
+    card.appendChild(cardTitle);
+    card.appendChild(subtitle);
+    card.appendChild(imgInfo);
+    imgInfo.appendChild(imgIcon);
+    imgInfo.appendChild(imgInfoText);
 
 
 }
